63444714cc96b4794590a7f8e04409ce93b2a01e
Peter Olsén <peter.olsen@conrecinfinity.com>
2024-03-24 15:30:52 +0100

    Added new page for tests

--- Lecture2/code/3_relative_locators.py
+++ Lecture2/code/3_relative_locators.py
@@ -9,26 +9,34 @@ from time import sleep
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("https://magento.softwaretestingboard.com")
+driver.get("https://magento.softwaretestingboard.com/customer/account/login")
+
+# https://www.selenium.dev/selenium/docs/api/py/webdriver_support/selenium.webdriver.support.relative_locator.html
 
 # above
-passwordField = driver.find_element(By.ID, "password")
-emailAddressField = driver.find_element(locate_with(By.TAG_NAME,  "input").above(passwordField))
+#passwordField = driver.find_element(By.ID, "pass")
+#emailAddressField = driver.find_element(locate_with(By.TAG_NAME,  "input").above(passwordField))
+
+#emailAddressField.send_keys('nisse.hult@hotmail.com')
 
 # below
-emailAddressField = driver.find_element(By.ID, "email")
-passwordField = driver.find_element(locate_with(By.TAG_NAME, "input").below(emailAddressField))
+#emailAddressField = driver.find_element(By.ID, "email")
+#passwordField = driver.find_element(locate_with(By.TAG_NAME, "input").below(emailAddressField))
+
+#passwordField.send_keys('SecretPassword')
 
 # to the left of
-submitButton = driver.find_element(By.ID, "submit")
-cancelButton = driver.find_element(locate_with(By.TAG_NAME, "button").to_left_of(submitButton))
+emailAddressField = driver.find_element(By.ID, "email")
+emailAddressLabel = driver.find_element(locate_with(By.TAG_NAME, "input").below(emailAddressField))
+
+signinButton.click()
 
 # to the right of
-cancelButton = driver.find_element(By.ID, "cancel")
-submitButton = driver.find_element(locate_with(By.TAG_NAME, "button").to_right_of(cancelButton))
+#cancelButton = driver.find_element(By.ID, "cancel")
+#submitButton = driver.find_element(locate_with(By.TAG_NAME, "button").to_right_of(cancelButton))
 
 # near
-emailAddressLabel = driver.find_element(By.ID, "lbl-email")
-emailAddressField = driver.find_element(locate_with(By.TAG_NAME, "input").near(emailAddressLabel))
-sleep(2)
+#emailAddressLabel = driver.find_element(By.ID, "lbl-email")
+#emailAddressField = driver.find_element(locate_with(By.TAG_NAME, "input").near(emailAddressLabel))
+sleep(5)
 driver.quit()
